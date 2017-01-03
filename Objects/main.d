915d421740595905d1a9999522b14752383a97ef
DamianSan <mdamian@student.agh.edu.pl>
2017-01-03 13:50:31 +0100

    Build logs/Drivers/Startups

---
+++ Objects/main.d
@@ -0,0 +1,8 @@
+.\objects\main.o: main.c
+.\objects\main.o: RGBW_LEDs_driver.h
+.\objects\main.o: D:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.12.0\Device\Include\MKL46Z4.h
+.\objects\main.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cm0plus.h
+.\objects\main.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmInstr.h
+.\objects\main.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmFunc.h
+.\objects\main.o: D:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.12.0\Device\Include\system_MKL46Z4.h
---
+++ Objects/rgbw_leds_driver.d
@@ -0,0 +1,8 @@
+.\objects\rgbw_leds_driver.o: RGBW_LEDs_driver.c
+.\objects\rgbw_leds_driver.o: RGBW_LEDs_driver.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.12.0\Device\Include\MKL46Z4.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cm0plus.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmInstr.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.1\CMSIS\Include\core_cmFunc.h
+.\objects\rgbw_leds_driver.o: D:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.12.0\Device\Include\system_MKL46Z4.h
